637e59ae14a7962a832ccce98886135fdd8f7674
Tina Ellis <tina.ellis@getg5.com>
2015-04-01 05:45:09 -0700

    formatting

--- book_exercises/oo_ruby.rb
+++ book_exercises/oo_ruby.rb
@@ -360,26 +360,26 @@
   # getter public, so joe.grade will raise an error. Create a better_grade_than? method,
   # that you can call like so...
 
-  class Student
-  def initialize(name, grade)
-    @name = name
-    @grade = grade
-  end
+    class Student
+    def initialize(name, grade)
+      @name = name
+      @grade = grade
+    end
 
-  def better_grade_than?(other_student)
-    grade > other_student.grade
-  end
+    def better_grade_than?(other_student)
+      grade > other_student.grade
+    end
 
-  protected
+    protected
 
-  def grade
-    @grade
-  end
-  end
+    def grade
+      @grade
+    end
+    end
 
-  joe = Student.new("Joe", 90)
-  bob = Student.new("Bob", 84)
-  puts "Well done!" if joe.better_grade_than?(bob)
+    joe = Student.new("Joe", 90)
+    bob = Student.new("Bob", 84)
+    puts "Well done!" if joe.better_grade_than?(bob)
 
   # 8) The problem is that the method hi is a private method, therefore it is unavailable
   # to the object. I would fix this problem by moving the hi method above the private reserved
